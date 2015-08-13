3f89f74227f77ee725adbc6d8ba46e5122c40eb7
Tony Stephens <tonystephens@gmail.com>
2015-08-13 13:17:52 -0400

    Cleaned up gulp file with config variables.

--- gulpfile.js
+++ gulpfile.js
@@ -2,148 +2,41 @@
 /*globals require */
 
 'use strict';
-// gulpfile.js
-var gulp        = require('gulp'),
+
+var gulp = require('gulp'),
     runSequence = require('run-sequence').use(gulp),
-    serve       = require('browser-sync'),
     gutil = require('gulp-util'),
+    serve = require('browser-sync'),
     jade = require('gulp-jade'),
     sass = require('gulp-sass'),
     del = require('del'),
     path = require('path'),
     http = require('http'),
     exec = require('child_process').exec,
-    rename = require("gulp-rename"),
+    rename = require('gulp-rename'),
     tree = require('ng-mural-patterns-tree'),
     scriptInject = require('ng-mural-patterns-inject'),
-    prunehtml = require('gulp-prune-html');
-
-
-// Commonly used tasks defined here.
-gulp.task('buildStyleGuide', function (callback) {
-    runSequence(
-        'clean',
-        'copy',
-        'injector',
-        'styleguideJsonTree',
-        'styleguideJadeIndex',
-        'styleguideJadeTemplates',
-        'styleguideMarkdownPatterns',
-        'sass',
-        'injector',
-        'copy',
-        callback
-    );
-});
+    prunehtml = require('gulp-prune-html'),
+    muralConfig = {
+        src: 'src/_mural/',
+        root: 'build/mural/',
+        dest: 'build/mural/patterns/',
+        css: '_assets/css/',
+        data: 'components/mural_data/',
+        patterns: 'components/mural_patterns/',
+        templates: 'components/mural_templates/templates/'
+    };
 
 
 /** -----------------------------------------------
- * SASS
+ * gulp serve - Start Local Server
  * --------------------------------------------- */
-gulp.task('sass', function () {
-    return gulp.src(['./src/_mural/_assets/sass/*.scss', '!./src/_mural/**/_*'])
-        .pipe(sass({
-            sourcemap: true
-        }))
-        .pipe(gulp.dest('./src/_mural/_assets/css'));
-});
-
-
-/** -----------------------------------------------
- * JSON Tree
- * --------------------------------------------- */
-gulp.task('styleguideJsonTree', function () {
-    gulp.src('./src/_mural/components/mural_patterns')
-        .pipe(tree({
-            patternsPath: './build/mural/patterns/components/mural_patterns/',
-            jsonPath: './src/_mural/components/mural_data'
-        }))
-        .pipe(gulp.dest('./src/_mural/components/mural_data/'));
-});
-
-
-/** -----------------------------------------------
- * INJECT JSON
- * --------------------------------------------- */
-gulp.task('injector', ['styleguideJsonTree'], function () {
-    gulp.src('./src/_mural/index.html')
-        .pipe(prunehtml(['#jsonPath']))
-        .pipe(scriptInject({
-            path: './src/_mural/components/mural_data',
-            varname: 'jsonPath'
-        }))
-        .pipe(gulp.dest('./src/_mural'));
-});
-
-
-/** -----------------------------------------------
- * Jade
- * --------------------------------------------- */
-gulp.task('styleguideJadeIndex', function () {
-    return gulp.src(['./src/_mural/components/mural_templates/index.jade'])
-        .pipe(jade({
-            pretty : true
-        }))
-        .pipe(gulp.dest('./src/_mural'));
-
-});
-gulp.task('styleguideMarkdownPatterns', function () {
-    return gulp.src(['!./src/_mural/components/mural_templates/**/*.jade', '!./src/_mural/components/mural_templates/templates', './src/_mural/components/mural_patterns'])
-        .pipe(jade({
-            pretty: true
-        }))
-        .pipe(rename(function (path) {
-            path.extname = ".md"
-        }))
-        .pipe(gulp.dest('./src/_mural/components/mural_patterns'));
-});
-gulp.task('styleguideJadeTemplates', function () {
-    return gulp.src(['./src/_mural/components/mural_templates/templates/*.jade'])
-        .pipe(jade({
-            pretty: true
-        }))
-        .pipe(gulp.dest('./build/mural/patterns/components/mural_templates/templates'));
-});
-
-/** -----------------------------------------------
- * Clean scripts
- * --------------------------------------------- */
-gulp.task('clean', function (callback) {
-    del([
-        './src/_mural/components/**/*.html',
-        './build/mural/patterns/**/*.*'
-    ], callback);
-});
-
-
-/** -----------------------------------------------
- * Copy
- * --------------------------------------------- */
-gulp.task('copy', [], function () {
-    return gulp.src(['./src/_mural/**', '!./src/_mural/components/**/*.jade'])
-        .pipe(gulp.dest('./build/mural/patterns'))
-});
-
-/** -----------------------------------------------
- * Watch Tasks
- * --------------------------------------------- */
-gulp.task('watch', function () {
-    gulp.watch('src/_mural/_assets/sass/**/*.scss', ['sass', 'copy']);
-    gulp.watch('src/_mural/components/mural_app/*.jade', ['styleguidePatterns', 'copy']);
-    gulp.watch('src/_mural/components/mural_patterns/**', ['styleguidePatterns', 'copy']);
-    gulp.watch('src/_mural/components/mural_patterns/**/*', ['styleguidePatterns', 'copy']);
-    gulp.watch(['src/_mural/components/mural_templates/**/*.jade'], ['styleguidePatterns', 'copy']);
-});
-
-
-
-
 gulp.task('serve', function () {
     serve({
         port: 3000,
         open: false,
         server: {
-            baseDir: 'build/mural',
+            baseDir: muralConfig.root,
             middleware: function (req, res, next) {
                 res.setHeader('Access-Control-Allow-Origin', '*');
                 next();
@@ -154,8 +47,119 @@ gulp.task('serve', function () {
 });
 
 /** -----------------------------------------------
- * Style Guide Build
+ * gulp buildStyleGuide - Style Guide Build
  * --------------------------------------------- */
-gulp.task('styleguidePatterns', ['styleguideJadeIndex', 'styleguideJadeTemplates', 'styleguideMarkdownPatterns', 'injector']);
-gulp.task('buildStyleGuide2', ['clean', 'injector', 'styleguideJadeIndex', 'styleguideJadeTemplates', 'styleguideMarkdownPatterns', 'sass', 'copy']);
+gulp.task('buildStyleGuide', function (callback) {
+    runSequence(
+        'clean',
+        'styleguideJsonTree',
+        'styleguideJadeIndex',
+        'styleguideJadeTemplates',
+        'styleguideMarkdownPatterns',
+        'injector',
+        'sass',
+        'copy',
+        callback
+    );
+});
+
+
+/** -----------------------------------------------
+ * gulp sass - SASS
+ * --------------------------------------------- */
+gulp.task('sass', function () {
+    return gulp.src(['./src/_mural/_assets/sass/*.scss', '!./src/_mural/**/_*'])
+        .pipe(sass({
+            sourcemap: true
+        }))
+        .pipe(gulp.dest(muralConfig.src + muralConfig.css));
+});
+
+
+/** -----------------------------------------------
+ * gulp styleguideJsonTree - JSON Tree
+ * @summary This reads through the folders of markdown documents in `Patterns` and generates a JSON file with paths
+ * --------------------------------------------- */
+gulp.task('styleguideJsonTree', function () {
+    gulp.src('./src/_mural/components/mural_patterns')
+        .pipe(tree({
+            patternsPath: './build/mural/patterns/components/mural_patterns/',
+            jsonPath: './src/_mural/components/mural_data'
+        }))
+        .pipe(gulp.dest(muralConfig.src + muralConfig.data));
+});
+
+
+/** -----------------------------------------------
+ * gulp injector - INJECT JSON
+ * @summary This reads the generated JSON files (from the Tree); and inserts those into the Index.
+ * --------------------------------------------- */
+gulp.task('injector', ['styleguideJsonTree'], function () {
+    gulp.src('./src/_mural/index.html')
+        .pipe(prunehtml(['#jsonPath']))
+        .pipe(scriptInject({
+            path: muralConfig.src + muralConfig.data,
+            varname: 'jsonPath'
+        }))
+        .pipe(gulp.dest(muralConfig.src));
+});
+
+
+/** -----------------------------------------------
+ * gulp styleguideJadeIndex - Hack for index file.
+ * --------------------------------------------- */
+gulp.task('styleguideJadeIndex', function () {
+    return gulp.src(['./src/_mural/components/mural_templates/index.jade'])
+        .pipe(jade({
+            pretty: true
+        }))
+        .pipe(gulp.dest(muralConfig.src));
+
+});
+/** -----------------------------------------------
+ * gulp styleguideMarkdownPatterns - Reads the markdown documents
+ * @todo research this module more.
+ * --------------------------------------------- */
+gulp.task('styleguideMarkdownPatterns', function () {
+    return gulp.src(['!./src/_mural/components/mural_templates/**/*.jade', '!./src/_mural/components/mural_templates/templates', './src/_mural/components/mural_patterns'])
+        .pipe(jade({
+            pretty: true
+        }))
+        .pipe(rename(function (path) {
+            path.extname = ".md"
+        }))
+        .pipe(gulp.dest(muralConfig.src + muralConfig.patterns));
+});
+/** -----------------------------------------------
+ * gulp styleguideJadeTemplates - Creates html templates cause I much prefer Jade
+ * --------------------------------------------- */
+gulp.task('styleguideJadeTemplates', function () {
+    return gulp.src(['./src/_mural/components/mural_templates/templates/*.jade'])
+        .pipe(jade({
+            pretty: true
+        }))
+        .pipe(gulp.dest(muralConfig.dest + muralConfig.templates));
+});
+
+/** -----------------------------------------------
+ * gulp clean - Clean scripts
+ * --------------------------------------------- */
+gulp.task('clean', function (callback) {
+    del([
+        './src/_mural/index.html',
+        './src/_mural/components/**/*.html',
+        './build/mural/patterns/**/*.*'
+    ], callback);
+});
+
+
+/** -----------------------------------------------
+ * gulp copy - Copy the files to the mural/patterns directory.
+ * --------------------------------------------- */
+gulp.task('copy', [], function () {
+    return gulp.src(['./src/_mural/**', '!./src/_mural/components/**/*.jade'])
+        .pipe(gulp.dest(muralConfig.dest));
+});
+
+
 
