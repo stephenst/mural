728a9c3598277914534978409751890e5a16b959
Tony Stephens <tonystephens@gmail.com>
2015-10-01 08:18:38 -0400

    Fixed Style Guide paths; now full Doc-Tool builds.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,42 +1,42 @@
-module.exports = function (grunt) {
-    'use strict';
-    require('load-grunt-tasks')(grunt);
-    grunt.loadNpmTasks('ng-mural-styleguide');
+module.exports = function(grunt) {
+    "use strict";
+    require("load-grunt-tasks")(grunt);
+    grunt.loadNpmTasks("ng-mural-styleguide");
 
     //Initializing Grunt Configuration
     grunt.initConfig({
         //  Read in the Package File
-        cfg: grunt.file.readJSON('config.json'),
+        cfg: grunt.file.readJSON("config.json"),
         styleguide: {
             build: {
                 options: {
                     framework: {
-                        name: 'styledocco',
+                        name: "styledocco",
                         options: {
-                            preprocessor: 'scss'
+                            preprocessor: "scss"
                         }
                     },
-                    name: 'Style Guide'
+                    name: "Style Guide"
                 },
                 files: {
-                    '<%= cfg.styles.dest %>': '<%= cfg.styles.assets %>'
+                    "<%= cfg.styles.root %>": "<%= cfg.styles.src %>"
                 }
             }
         },
         jsdoc: {
             dist: {
-                jsdoc: '/usr/local/bin/jsdoc',
+                jsdoc: "/usr/local/bin/jsdoc",
                 src: [
-                    '<%= cfg.jsdoc.src %><%= cfg.jsdoc.sourceFiles %>',
-                    './readme.md'
+                    "<%= cfg.jsdoc.src %><%= cfg.jsdoc.sourceFiles %>",
+                    "./readme.md"
                 ],
                 options: {
-                    destination: '<%= cfg.jsdoc.dest %>',
-                    configure: './node_modules/ng-mural-jsdoc/conf.json',
-                    template: './node_modules/ng-mural-jsdoc/template'
+                    destination: "<%= cfg.jsdoc.dest %>",
+                    configure: "./node_modules/ng-mural-jsdoc/conf.json",
+                    template: "./node_modules/ng-mural-jsdoc/template"
                 }
             }
         }
     });
-    grunt.registerTask('default', ['styleguide', 'jsdoc']);
+    grunt.registerTask("default", ["styleguide", "jsdoc"]);
 };
